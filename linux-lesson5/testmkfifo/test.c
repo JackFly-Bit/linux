d7ff77d657b471d850b8196a816d5ba39fdcde07
JackFly-Bit <958141774@qq.com>
2021-02-04 18:05:08 +0800

    进程间通信3

---
+++ linux-lesson5/testmkfifo/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int main()
+{
+  int ret = mkfifo("./fifo_test", 0664);
+  if (ret < 0)
+  {
+    perror("mkfifo");
+    return -1;
+  }
+  //TODO
+  //open
+  //read
+  //write
+  printf("create success\n");
+  return 0;
+}
